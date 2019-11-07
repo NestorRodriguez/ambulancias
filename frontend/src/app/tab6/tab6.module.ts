746bcb4480d111e6d0efe7b56d4fe4ad5939bca0
CarlosVergara888 <carlos_andresvp@hotmail.com>
2019-11-06 21:41:41 -0500

    tab6 y tab7 mejorado

--- frontend/src/app/tab6/tab6.module.ts
+++ frontend/src/app/tab6/tab6.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AgmCoreModule } from '@agm/core';
 import { IonicModule } from '@ionic/angular';
 
 import { Tab6Page } from './tab6.page';
@@ -19,8 +19,11 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    RouterModule.forChild([{ path: '', component: Tab6Page }])
   ],
   declarations: [Tab6Page]
 })
 export class Tab6PageModule {}
+
+
